9856c2dafc99f80fbc870267f121b6bdcad631fd
deus <deus@COVID-BROADCASTER.>
2024-11-24 02:43:30 -0600

    Changed to port 80

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path")
 
 dotenv.config()
 const MONGO_URI = process.env.MONGO_URI
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 const app = express()
 
