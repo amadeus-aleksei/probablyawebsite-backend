8439cf65a9262b415867eab9afb13bd5e62ddc01
deus <deus@COVID-BROADCASTER.>
2024-11-25 21:46:44 -0600

    test events in server.js

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(
     })
 );
 
+const eventsRoutes = require("./routes/events");
+app.use("/api/events", eventsRoutes);
+
 const tasksRoutes = require("./routes/tasks");
 app.use("/api/tasks", tasksRoutes);
 
-const eventsRoutes = require("./routes/events");
-app.use("/api/events", eventsRoutes);
+
 
 // Catch-all route for serving the frontend (only if required in production)
 if (process.env.NODE_ENV === "production") {
