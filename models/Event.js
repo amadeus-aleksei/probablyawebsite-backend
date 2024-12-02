e095b89be7c041c61eb0490fa3e1bc4039314c4c
deus <deus@COVID-BROADCASTER.>
2024-12-02 00:27:23 -0600

    Fixing prod events issue

--- models/Event.js
+++ models/Event.js
@@ -2,8 +2,22 @@ const mongoose = require("mongoose")
 
 const eventSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    start: {type: Date, required: true},
-    end: {type: Date, required: true},
+    start: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: (value) => !isNaN(Date.parse(value)),
+            message: "Invalid date format for start",
+        },
+    },
+    end: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: (value) => !isNaN(Date.parse(value)),
+            message: "Invalid date format for end",
+        },
+    },
     allDay: {type: Boolean, default: false},
     description: {type: String},
     location: {type: String},
